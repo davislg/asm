e62a604ba565f30f1547fa3bb53457c5650a7eb0
davis <lawrence.davis@gmail.com>
2012-05-03 14:02:03 -0600

    Added Makefile for hello

---
+++ hello/osx/hello.asm
@@ -0,0 +1,24 @@
+section .text
+  global start
+
+start:
+
+  push dword len
+	push dword msg
+	push dword 1
+
+	mov eax,0x4
+	sub esp, 0x4
+	int 0x80
+
+	add esp, 0x10
+
+	push dword 0x0
+
+	mov eax, 0x1
+	sub esp, 0x4
+	int 0x80
+
+section .data
+  msg db "hello, world", 0xA
+	len equ $-msg
